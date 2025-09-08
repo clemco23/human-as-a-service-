019114d004f4ffa9ab0e3332ee32f0676021ebf9
Tidragon <theomoon77@gmail.com>
2025-09-08 23:02:17 +0200

    V1 of the human presentation cards

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,16 @@
 import Button from "../components/atoms/button"
+import CardsPart from '../components/organismes/cardsPart'
+
+
 
 export default function Home() {
     return (
         <div>
             <h1 className="text-3xl text-blue-500 mb-4">Home</h1>
             <Button size="large" color="primary" href="https://google.com">Google</Button>
-
+            <div className="mt-8 mx-4">
+                <CardsPart />
+            </div>
         </div>
     )
 }
