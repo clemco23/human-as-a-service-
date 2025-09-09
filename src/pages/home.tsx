0e293f1c7423c9c8d33985d0a5f5eae810031b94
Tidragon <theomoon77@gmail.com>
2025-09-09 10:10:18 +0200

    last modif

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,4 @@
 import Button from "../components/atoms/button"
-import CardsPart from '../components/organismes/cardsPart'
 
 
 
@@ -8,9 +7,6 @@ export default function Home() {
         <div>
             <h1 className="text-3xl text-blue-500 mb-4">Home</h1>
             <Button size="large" color="primary" href="https://google.com">Google</Button>
-            <div className="mt-8 mx-4">
-                <CardsPart />
-            </div>
         </div>
     )
 }
