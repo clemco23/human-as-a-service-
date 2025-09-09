d4f4a976b69bea5a07b92733d2b99ef2ec525836
Tidragon <theomoon77@gmail.com>
2025-09-09 15:02:59 +0200

    V2 Adding human data with Json

--- src/components/organismes/cardsPart.tsx
+++ src/components/organismes/cardsPart.tsx
@@ -1,45 +1,25 @@
-import Cards from '../molecules/humanCard'
-import Data from '../../data/human/dataHuman.json'
+import Cards from '../molecules/humanCard';
+import Data from '../../data/human/dataHuman.json';
 
-export default function CardsPart() {
-    return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
-            {Data.map((person, index) => (
-                <Cards
-                    key={index}
-                    title={person.title}
-                    age={person.age}
-                    personality={person.personality}
-                    description={person.description}
-                    search={person.search}
-                />
-            ))}
-        </div>
-    )
-    // return (
-    //     <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20"><Cards
-    //         title="John Doe"
-    //         age={30}
-    //         personality="Friendly"
-    //         description="A friendly person who enjoys teamwork, mentoring juniors, and creating positive work environments."
-    //         search="Full-stack developer, React, Node.js, team player, puis blabla merde j'ia pas d'inspii il est tard là"
-    //     />
+type Props = {
+  data: typeof Data;
+};
 
-    //         <Cards
-    //             title="Jane Smith"
-    //             age={25}
-    //             personality="Creative"
-    //             description="A creative individual with a passion for UI/UX design, prototyping, and building visually engaging experiences."
-    //             search="UX designer, Figma, Adobe XD, creative thinker,puis blabla merde j'ia pas d'inspii il est tard là"
-    //         />
-
-    //         <Cards
-    //             title="Mike Johnson"
-    //             age={40}
-    //             personality="Analytical"
-    //             description="An analytical thinker experienced in project management, process optimization, and data-driven decision making."
-    //             search="Project manager, Agile, data analysis, leadership, puis blabla merde j'ia pas d'inspii il est tard là"
-    //         />
-    //     </div>
-    // )
+export default function CardsPart({ data }: Props) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mx-20">
+      {data.map((person, index) => (
+        <Cards
+          key={index}
+          id={person.id}
+          title={person.title}
+          age={person.age}
+          personality={person.personality}
+          description={person.description}
+          search={person.search}
+        />
+      ))}
+    </div>
+  );
 }
+
