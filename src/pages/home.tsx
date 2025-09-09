b397fc1a9e1c7adcf1fcf7d97dad678b782387b0
clement <clementboscher6@gmail.com>
2025-09-09 11:20:10 +0200

    update home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,17 +1,6 @@
 // import Button from "../components/atoms/button"
 import Header from "../components/molecules/header"
 
-// export default function Home() {
-//     return (
-//         <div>
-//             <Header />
-//             <main className="p-8">
-//                 <h1 className="text-3xl text-blue-500 mb-4">Home</h1>
-//                 <Button size="large" color="primary" href="https://google.com">Google</Button>
-//             </main>
-//         </div>
-//     )
-// }
 
 import Button from "../components/atoms/button"
 import { useState } from "react";
